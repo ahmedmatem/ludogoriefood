9f2b335b7a8f3b85637d1dbc42b43c2a4bba76c8
ahmed <ahmedmatem@gmail.com>
2016-03-04 19:57:26 +0000

    Integrated Repository pattern

---
+++ source/web/common/LudogorieFood.Web.Common/Models/IAuditInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace LudogorieFood.Web.Common.Models
+{
+    using System;
+
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ source/web/common/LudogorieFood.Web.Common/Models/IDeletableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace LudogorieFood.Web.Common.Models
+{
+    using System;
+
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
