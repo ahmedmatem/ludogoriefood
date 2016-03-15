7a68f420b37decdb312c6135a1d978d76c98b44f
ahmed <ahmedmatem@gmail.com>
2016-03-15 19:46:11 +0000

    Improve slider in home page

--- source/LudogorieFood.Web/Controllers/HomeController.cs
+++ source/LudogorieFood.Web/Controllers/HomeController.cs
@@ -1,12 +1,33 @@
 ﻿namespace LudogorieFood.Web.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
+
+    using Common;
+    using Models;
+    using ViewModels.Home;
+    using Infrastructure;
 
     public class HomeController : Controller
     {
+        public IDbRepository<Slide> slides;
+
+        public HomeController(IDbRepository<Slide> slides)
+        {
+            this.slides = slides;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var activeSlides = this.slides.All().ToList();
+            var linkedSlides = new LinkedSlideList(activeSlides);
+
+            var model = new HomePageViewModel
+            {
+                Slides = linkedSlides.Slides,
+            };
+
+            return View(model);
         }
     }
 }
---
+++ source/LudogorieFood.Web/ViewModels/Home/HomePageViewMmodel.cs
@@ -0,0 +1,11 @@
+﻿namespace LudogorieFood.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    using LudogorieFood.Models;
+
+    public class HomePageViewModel
+    {
+        public ICollection<Slide> Slides { get; set; }
+    }
+}
